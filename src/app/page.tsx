ad6af6644b134380a79349d60cb6476b5da0ad98
Ranjula Ilukpitiya <ranjulailukpitiya@gmail.com>
2026-03-12 15:16:54 +0530

    hero section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import HeroSection from '@/components/hero/HeroSection';
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-transparent">
-      <h1 className="text-6xl font-extrabold bg-clip-text text-transparent bg-linear-to-r from-blue-500 via-indigo-600 to-teal-400">
-        aba ceylon
-      </h1>
+    <div className="min-h-screen">
+      <HeroSection />
     </div>
   );
 }
